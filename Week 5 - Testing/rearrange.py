287d2782064008aee04913e4f91e9027987153f8
dke-a <angeles.don@yahoo.com>
2023-06-07 16:48:47 -0400

    add notes for unit tests section of week 5.

--- Week 5 - Testing/rearrange.py
+++ Week 5 - Testing/rearrange.py
@@ -2,4 +2,6 @@ import re
 
 def rearrange_name(name):
     result = re.search(r"^([\w .]*), ([\w .]*)$", name)
-    return f"{result[2]},{result[1]}"
+    if result is None:
+        return name
+    return f"{result[2]} {result[1]}"
